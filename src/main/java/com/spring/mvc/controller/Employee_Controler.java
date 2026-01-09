5db23b233fe43b9a1cd324e945a0d08671c9d861
Souvik7602 <sm2496444@gmail.com>
2026-01-10 05:22:14 +0530

    update

--- src/main/java/com/spring/mvc/controller/Employee_Controler.java
+++ src/main/java/com/spring/mvc/controller/Employee_Controler.java
@@ -1,95 +1,22 @@
 package com.spring.mvc.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.spring.mvc.model.Employee;
 
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class Employee_Controler {
-	Map<Integer,Employee> map=new HashMap<>();
-	int i=0;
+	
 	@RequestMapping("/emp")
-	public String greetings() {
+	@ResponseBody
+	public Employee greetings() {
 		System.out.println("running............");
-		return "employe-form";
+		Employee kk=new Employee(1,51,"kk");
+		return kk;
 	}
 	
-//	@RequestMapping("/saveEmp")
-//	public String form(@RequestParam("id")int id,@RequestParam("name")String name ,@RequestParam("age")int age,Model model) {
-//		System.out.println("running............");
-//		Employee employee=new Employee(id,name,age);
-//		map.put(employee.getId(),employee);
-//		model.addAttribute("emp", employee);
-//		return "employe-profile";
-//	}
-	
-	@PostMapping("/saveEmp")
-	public String form(@ModelAttribute Employee employee,Model model) {
-		System.out.println("running............");
-		employee.setId(++i);
-		map.put(employee.getId(),employee);
-		model.addAttribute("emp", employee);
-		return "employe-profile";
-	}
-	
-	@GetMapping("/getEmp/{id}")
-	public String getEmpByid(@PathVariable("id")int id,Model model) {
-		System.out.println("getemp............"+id);
-		Employee employee=map.get(id);
-		model.addAttribute("emp", employee);
-		return "employe-profile";
-	}
-	
-	@GetMapping("/removeEmp/{id}")
-	public String remove(@PathVariable("id")int id,Model model) {
-		System.out.println("remove............"+id);
-		Employee employee=map.remove(id);
-		model.addAttribute("emp", employee);
-		return "employe-profile";
-	}
-	
-	@GetMapping("/geJSESSIONID")
-	public String sessionTracing(@CookieValue("JSESSIONID") String jsessionid) {
-		System.out.println("Session id is............"+jsessionid);
-
-		return "employe-profile";
-	}
-	
-	@GetMapping("/setCookie")
-	public String learningCookie(HttpServletResponse response) {
-
-		Cookie cookie = new Cookie("nikhil", "SanvedansheelData");
-
-		cookie.setMaxAge(3600);
-
-		response.addCookie(cookie);
-
-		return "redirect:/getCookie";
-	}
-
-	@GetMapping("/getCookie")
-	public String learningSessionTracking(@CookieValue(name = "nikhil", defaultValue = "defaultVal") String cookie,
-			Model model) {
-
-		System.out.println("Your Session id is: " + cookie);
-		model.addAttribute("myCookie", cookie);
-
-		return "cookie_data";
-	}
-
 } 
 
--- src/main/java/com/spring/mvc/model/Employee.java
+++ src/main/java/com/spring/mvc/model/Employee.java
@@ -1,50 +1,12 @@
 package com.spring.mvc.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Employee {
 	private int id;
 	private int age;
 	private String name;
-	 
-	
-	public Employee() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	public Employee(int id,String name,int age) {
-		super();
-		this.name = name;
-		this.id=id;
-		this.age = age;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public int getAge() {
-		return age;
-	}
-
-	public void setAge(int age) {
-		this.age = age;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	@Override
-	public String toString() {
-		return "Employee [id=" + id + ", age=" + age + ", name=" + name + "]";
-	}
-
 }
